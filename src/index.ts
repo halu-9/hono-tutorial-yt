2cb9122b345a8961a12bbdeea681e1281a1a37cc
ryo <>
2024-09-30 17:00:14 +0900

    ディレクトリのリファクタリング実行

--- src/index.ts
+++ src/index.ts
@@ -1,82 +1,19 @@
 import { Hono } from "hono";
-import { prettyJSON } from "hono/pretty-json";
+import posts from "./blogs/blogs";
+import auth from "./auth/auth";
+import { basicAuth } from "hono/basic-auth";
 
 const app = new Hono();
 
-let blogPosts = [
-  {
-    id: "1",
-    title: "Blog1",
-    content: "Blog1 Posts",
-  },
-  {
-    id: "2",
-    title: "Blog2",
-    content: "Blog2 Posts",
-  },
-  {
-    id: "3",
-    title: "Blog3",
-    content: "Blog3 Posts",
-  },
-];
+app.use(
+  "/auth/*",
+  basicAuth({
+    username: "hono",
+    password: "acoolproject",
+  })
+);
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
-});
-
-app.get("/posts", (c) => c.json({ posts: blogPosts }));
-
-app.get("/posts/:id", (c) => {
-  const id = c.req.param("id");
-  const post = blogPosts.find((p) => p.id === id);
-
-  if (post) {
-    return c.json(post);
-  } else {
-    return c.json({ message: "not found this page" }, 404);
-  }
-});
-
-// 投稿（post）
-app.post("/posts", async (c) => {
-  const { title, content } = await c.req.json<{
-    title: string;
-    content: string;
-  }>();
-  const newPost = { id: String(blogPosts.length + 1), title, content };
-  blogPosts = [...blogPosts, newPost];
-
-  return c.json(newPost, 201);
-});
-
-// 更新(put)
-app.put("/posts/:id", async (c) => {
-  const id = c.req.param("id");
-  const index = blogPosts.findIndex((p) => p.id === id);
-
-  if (index === -1) {
-    return c.json({ message: "Post not found" }, 404);
-  }
-
-  const { title, content } = await c.req.json();
-  blogPosts[index] = { ...blogPosts[index], title, content };
-
-  return c.json(blogPosts[index]);
-});
-
-// 削除(delete)
-app.delete("/posts/:id", async (c) => {
-  const id = c.req.param("id");
-  const index = blogPosts.findIndex((p) => p.id === id);
-
-  if (index === -1) {
-    return c.json({ message: "Post not found" }, 404);
-  }
-
-  blogPosts = blogPosts.filter((p) => p.id !== id);
-
-  return c.json({ message: "Blog Post Deleted" });
-});
+app.route("/posts", posts);
+app.route("/auth", auth);
 
 export default app;
